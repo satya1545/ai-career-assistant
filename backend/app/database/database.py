afee6ff74bfbf6f2a776970858339e4772a2e116
Satyanarayana <satyanarayanapasupunuri@gmail.com>
2026-07-28 20:53:10 +0530

    Restore development Environment

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config.config import settings
 
+
+print("DATABASE_URL =", settings.DATABASE_URL)
+
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
