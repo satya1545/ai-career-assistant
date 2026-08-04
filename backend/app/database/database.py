1073c6e2bf6fb549f8e94a700aee01b4a7671ab0
Satyanarayana <satyanarayanapasupunuri@gmail.com>
2026-08-04 19:51:59 +0530

    Update API for a production

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config.config import settings
-
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
