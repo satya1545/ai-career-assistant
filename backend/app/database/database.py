9c6a1c08bde4df381576d89c9c9eb1b69e035c01
Satyanarayana <satyanarayanapasupunuri@gmail.com>
2026-08-04 20:34:18 +0530

    Fix DATABASE_URL

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config.config import settings
+
 engine = create_engine(
-    DATABASE_URL,
+    settings.DATABASE_URL,
     pool_pre_ping=True
 )
 
